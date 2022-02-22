a5e518a2c5e7eecd3cf7e2475925283edb74d392
b-tudor <btudor@mail.usf.edu>
2022-02-22 14:34:18 -0500

    init

---
+++ absurdle.cpp
@@ -0,0 +1,11 @@
+// Absurdle-Abs.cpp
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string word_list[] = { "ABACK", "ABBOT", "ABOUT", "ADMIN", "ADMIT", "ADULT", "AFFIX", "VIGOR", "VIRAL", "WARTY", "WHARF", "WHIRL", "WORDY", "WORLD", "WORRY", "WORTH" };
+    std::cout << "Testing... 1 2 3 4 5 6\n";
+}
